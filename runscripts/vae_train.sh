705cc82f54d1511739a912040ac6caedd3b2ab0f
bioatmosphere <wbwenwu@gmail.com>
2025-05-18 06:10:01 -0700

    slurm job script

--- runscripts/vae_train.sh
+++ runscripts/vae_train.sh
@@ -2,12 +2,15 @@
 #SBATCH --job-name=gem
 #SBATCH -A m2467
 #SBATCH -C gpu
-#SBATCH -q shared
+#SBATCH -q shared #regular
 #SBATCH -t 24:00:00
 #SBATCH -n 1
 #SBATCH -c 32
 #SBATCH --gpus-per-task=1
 
+module load python
+conda activate genai
+export logdir=/pscratch/sd/w/wangb/pytorch_genai/run
 cd ../
-python train.py --model vae --gpus 1
+python train.py --model vae --epochs 457 --batch-size 128 --logdir $logdir --gpus 1
 
